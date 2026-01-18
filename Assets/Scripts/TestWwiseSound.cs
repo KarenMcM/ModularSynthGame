b108f043993c50e97e37363f21627eefd78c1dd1
Karen McMullan <karen@imposter-games.com>
2026-01-17 16:49:32 -0800

    Unity assets

---
+++ Assets/Scripts/TestWwiseSound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestWwiseSound : MonoBehaviour
+{
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            AkSoundEngine.PostEvent("Play_Mysterious_Atmos", gameObject);
+        }
+    }
+}
